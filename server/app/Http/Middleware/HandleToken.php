8093e59cacbba17ec1058a575ca33eed3f4924d5
Ahamed Rasni Nathees <rasninathees.se@gmail.com>
2024-08-11 00:38:37 +0530

    refactor(auth): store access token in httponly cookie server side

---
+++ server/app/Http/Middleware/HandleToken.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class HandleToken
+{
+    /**
+     * Handle an incoming request.
+     * Need to enable secure cookie when deploy this on production
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $response = $next($request);
+
+        $content = $response->getContent();
+        $json = json_decode($content, true);
+        $access_token = $json['access_token'] ?? null;
+        $refresh_token = $json['refresh_token'] ?? null;
+        $expires_in = $json['expires_in'] ?? 3600;
+
+        if ($access_token) {
+            $cookie_access = cookie('access_token', $access_token, $expires_in / 60, httpOnly: true, sameSite: 'Lax');
+            $cookie_refresh = cookie('refresh_token', $refresh_token, 60 * 24 * 30, httpOnly: true, sameSite: 'Lax');
+            $response = response($response->getContent())->withCookie($cookie_refresh, $cookie_access);
+        }
+
+        return $response;
+    }
+}
