59d66cfab7515ef17fad3daf8c393380e3020397
Ahamed Rasni Nathees <rasninathees.se@gmail.com>
2024-08-10 22:23:52 +0530

    test(auth): add  tests for login feature

---
+++ server/tests/Feature/Auth/UserLoginTest.php
@@ -0,0 +1,69 @@
+<?php
+use Laravel\Passport\Client;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+it('Session has errors when submit empty form', function () {
+   
+
+
+
+    $client = Client::factory()->create([
+        'redirect' => 'http://localhost',
+        'personal_access_client' => false,
+        'password_client' => true,
+        'revoked' => false,
+    ]);
+
+    $data = [
+        'grant_type' => 'password',
+        'client_id' => $client->id, // Use id instead of name
+        'client_secret' => $client->secret,
+        'username' =>'' ,
+        'password' =>'',
+        'scope' => '*',
+    ];
+
+    $this->post('/api/oauth/token', $data)
+       ->assertSessionHasErrors(['username','password']);
+
+
+
+
+});
+
+it('can successfully get token', function () {
+    $user = User::factory()->create([
+        'password' => Hash::make('test-password'),
+    ]);
+
+
+
+    $client = Client::factory()->create([
+        'redirect' => 'http://localhost',
+        'personal_access_client' => false,
+        'password_client' => true,
+        'revoked' => false,
+    ]);
+
+    $data = [
+        'grant_type' => 'password',
+        'client_id' => $client->id, // Use id instead of name
+        'client_secret' => $client->secret,
+        'username' => $user->email,
+        'password' => 'test-password',
+        'scope' => '*',
+    ];
+
+    $this->post('/api/oauth/token', $data)
+        ->assertStatus(200)
+        ->assertJsonStructure([
+            'token_type',
+            'expires_in',
+            'access_token',
+        ]);
+
+
+
+
+});
