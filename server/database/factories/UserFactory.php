6211f94258adefb7f8f90ba436f6d4158a79dc31
Ahamed Rasni Nathees <rasninathees.se@gmail.com>
2024-08-13 00:14:03 +0530

    feat(user prfile): add user profile table

--- server/database/factories/UserFactory.php
+++ server/database/factories/UserFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\UserProfile;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
@@ -24,11 +25,11 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
+           
         ];
     }
 
