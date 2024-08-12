a66bce13b34adc186e852985fcedbf50f9ea5880
Ahamed Rasni Nathees <rasninathees.se@gmail.com>
2024-08-12 19:27:05 +0530

    refactor(auth):handle errors

--- client/src/helper/HandleErrors.ts
+++ client/src/helper/HandleErrors.ts
@@ -1,13 +1,20 @@
 import { toast } from 'vue3-toastify'
 import 'vue3-toastify/dist/index.css'
+import type { object } from 'yup'
 
+interface CustomErrorResponse {
+  response: {
+    data: {
+      message: string
+    }
+  }
+}
 const handleError = (error: unknown): void => {
-  if (
-    error &&
-    typeof error === 'object' &&
-    (error as { response?: unknown }).response === undefined
-  ) {
+  if (error && typeof error === 'object' && (error as CustomErrorResponse).response === undefined) {
     toast.error('Please check your internet connection or wait until servers are back online.')
+  } else if (error && typeof error === 'object' && (error as CustomErrorResponse).response) {
+    const message = (error as CustomErrorResponse).response.data.message
+    toast.error(message)
   }
 }
 
